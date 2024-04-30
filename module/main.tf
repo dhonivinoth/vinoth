002cb595becdc8675feba1b326cc1a2f98cff813
professionalazure <professionalazuremail@gmail.com>
2024-04-30 17:54:14 +0000

    Updated main.tf

--- module/main.tf
+++ module/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_monitor_metric_alert" "cpu_alert" {
 
   criteria {
     metric_namespace = "Microsoft.Web/sites"
-    metric_name      = "CpuUsagePercentage"
+    metric_name      = "AverageCpuPercentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = 80

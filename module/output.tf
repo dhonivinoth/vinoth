a7ea060f4e342ebb545abf727170ae8b968cf37b
professionalazure <professionalazuremail@gmail.com>
2024-05-01 09:18:19 +0000

    Updated output.tf

--- module/output.tf
+++ module/output.tf
@@ -1,1 +1,11 @@
-##
+output "cpu_alert_id" {
+  value = azurerm_monitor_metric_alert.cpu_alert.id
+}
+
+output "memory_alert_id" {
+  value = azurerm_monitor_metric_alert.memory_alert.id
+}
+
+output "response_time_alert_id" {
+  value = azurerm_monitor_metric_alert.response_time_alert.id
+}

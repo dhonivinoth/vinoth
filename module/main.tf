2b7f7ee59af7e06219c2396b25f261c490aeaa93
professionalazure <professionalazuremail@gmail.com>
2024-05-01 07:28:26 +0000

    Updated main.tf

--- module/main.tf
+++ module/main.tf
@@ -60,8 +60,7 @@ resource "azurerm_monitor_metric_alert" "cpu_alert" {
     aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = 90
-    time_aggregation = "PT1M"
-    time_window      = "PT15M"
+    time_aggregation = "PT15M"
   }
 
   action {
@@ -127,8 +126,7 @@ resource "azurerm_monitor_metric_alert" "ssl_certificate_alert" {
     aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = "86400"
-    time_aggregation = "PT1M"
-    time_window      = "PT15M"
+    time_aggregation = "PT1H"
   }
 
   action {
